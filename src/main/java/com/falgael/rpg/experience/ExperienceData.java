defffbc48219e2ef64241163ca560de0af97448c
falgael <ole.brenner24@gmail.com>
2023-07-21 19:42:19 +0200

    Class definitions for Experience implementation

---
+++ src/main/java/com/falgael/rpg/experience/ExperienceData.java
@@ -0,0 +1,9 @@
+package com.falgael.rpg.experience;
+
+/**
+ * Holds all Experience data
+ * @author falgael
+ * @version 0.0.1
+ */
+public class ExperienceData {
+}
---
+++ src/main/java/com/falgael/rpg/experience/ExperienceHandler.java
@@ -0,0 +1,9 @@
+package com.falgael.rpg.experience;
+
+/**
+ * Manages the experience of all Players
+ * @author falgael
+ * @version 0.0.1
+ */
+public class ExperienceHandler {
+}
