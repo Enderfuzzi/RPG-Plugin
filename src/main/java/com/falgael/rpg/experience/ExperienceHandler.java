f27a64a445d6a7672cbbc3295f4779fe13454f01
falgael <ole.brenner24@gmail.com>
2023-07-23 00:31:47 +0200

    Reduced amount of xp for a crafting recipe. Further only small non-functional changes and refactoring. Added javadoc.

--- src/main/java/com/falgael/rpg/experience/ExperienceHandler.java
+++ src/main/java/com/falgael/rpg/experience/ExperienceHandler.java
@@ -14,6 +14,7 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * Manages the experience of all Players
@@ -25,6 +26,13 @@ public class ExperienceHandler implements Listener {
     /** Assigns Experience data to each player */
     private static HashMap<Player,ExperienceData> experiences = new HashMap<>();
 
+    /**
+     * Method to send a formatted Message for the experience increase
+     * @param player The player to target
+     * @param proficiencyRepresentation The representation of the proficiency
+     * @param currentExperience The current Experience of the {@code Player}
+     * @param currentExperienceBorder The current Experience Border of the {@code Player}
+     */
     private void experienceIncreaseMessage(@NotNull Player player, String proficiencyRepresentation, int currentExperience, int currentExperienceBorder) {
         TextComponent message = new TextComponent(ChatColor.GOLD + "" + ChatColor.ITALIC + proficiencyRepresentation + ": " + currentExperience + "/" + currentExperienceBorder + " Xp");
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR,message);
@@ -53,11 +61,15 @@ public class ExperienceHandler implements Listener {
             }
         }
     }
+
+    /**
+     * Searches each block craft experience of a {@code Player} when crafting a block. Works with crafting whole stacks with shift-click.
+     * @param event describes the {@code CraftItemEvent}
+     */
     @EventHandler
     public void onCraft(@NotNull CraftItemEvent event) {
-        if (!(event.getWhoClicked() instanceof Player)) return;
-        Player player = (Player) event.getWhoClicked();
-        Bukkit.getLogger().info("Crafting result name: " + event.getCurrentItem().getType().toString());
+        if (!(event.getWhoClicked() instanceof Player player)) return;
+        Bukkit.getLogger().info("Crafting result name: " + Objects.requireNonNull(event.getCurrentItem()).getType());
         Bukkit.getLogger().info("Size of crafting: " + event.getCurrentItem().getAmount());
 
 
@@ -73,10 +85,10 @@ public class ExperienceHandler implements Listener {
                             craftedAmount = is.getAmount();
                         }
                     }
-                    Bukkit.getLogger().info("Amount to craft: " + craftedAmount);
+                    Bukkit.getLogger().info("Amount to craft with shift: " + craftedAmount);
                 }
 
-                ef.increaseExperience(ef.amountBlockCraftingExperience(event.getCurrentItem().getType()) * event.getCurrentItem().getAmount() * craftedAmount);
+                ef.increaseExperience(ef.amountBlockCraftingExperience(event.getCurrentItem().getType()) * craftedAmount);
                 experienceIncreaseMessage(player,ef.getProficiencyRepresentation(),ef.getCurrentExperience(),ef.getCurrentExperienceBorder());
             }
         }
