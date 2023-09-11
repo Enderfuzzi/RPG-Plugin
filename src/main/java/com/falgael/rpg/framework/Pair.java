1aa3e445fb2e094ac3a2a1e9c5b4fb8b41cfb54d
falgael <ole.brenner24@gmail.com>
2023-09-12 00:40:58 +0200

    Build Pair to record

--- src/main/java/com/falgael/rpg/framework/Pair.java
+++ src/main/java/com/falgael/rpg/framework/Pair.java
@@ -1,21 +1,5 @@
 package com.falgael.rpg.framework;
 
-public class Pair <T, V>{
-
-    private final T first;
-    private final V second;
-
-    public Pair(T first,V second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public T getFirst() {
-        return first;
-    }
-
-    public V getSecond() {
-        return second;
-    }
+public record Pair<T, V>(T first, V second) {
 
 }
