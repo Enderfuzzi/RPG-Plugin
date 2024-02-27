b163ba4877813dc082f4162c4af1704aa36df3c3
falgael <ole.brenner24@gmail.com>
2024-02-27 17:58:19 +0100

    Added Villager 1-3

--- src/main/java/com/falgael/rpg/definitions/woodwork/Villager.java
+++ src/main/java/com/falgael/rpg/definitions/woodwork/Villager.java
@@ -1,6 +1,7 @@
 package com.falgael.rpg.definitions.woodwork;
 
 import com.falgael.rpg.items.ItemManagement;
+import com.falgael.rpg.items.ItemStackManipulation;
 import com.falgael.rpg.proficiency.Proficiency;
 import com.falgael.rpg.proficiency.Rarity;
 import com.falgael.rpg.villager.MainVillagerDefinition;
@@ -8,6 +9,7 @@ import com.falgael.rpg.villager.MerchantRecipeBuilder;
 import com.falgael.rpg.villager.VillagerInstance;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.MerchantRecipe;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,46 +29,91 @@ public class Villager extends MainVillagerDefinition {
                         .rarity(Rarity.SIMPLE)
                         .proficiency(Proficiency.WOODWORK)
                         .level(1)
-                        .add(new MerchantRecipeBuilder(new ItemStack(Material.ACACIA_LOG,16)).addIngredients(new ItemStack(Material.ACACIA_LOG,16)).create())
-                        /*
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.ACACIA_LOG,16)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.BAMBOO,32)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.BIRCH_LOG,16)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.CHERRY_LOG,16)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.CRIMSON_STEM,16)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.JUNGLE_LOG,16)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.MANGROVE_LOG,16)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.OAK_LOG,16)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.SPRUCE_LOG,16)).create())
-                        .add(new MerchantRecipeBuilder(Items.SILVER_COIN.getItemByKey()).addIngredients(new ItemStack(Material.WARPED_STEM,16)).create())
+
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.ACACIA_LOG, 16)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.BAMBOO, 32)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.BIRCH_LOG, 16)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.CHERRY_LOG, 16)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.CRIMSON_STEM, 16)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.JUNGLE_LOG, 16)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.MANGROVE_LOG, 16)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.OAK_LOG, 16)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.SPRUCE_LOG, 16)).create())
+                        .add(new MerchantRecipeBuilder(itemManager.getItemByID(90000).getItemStackRepresentation())
+                                .addIngredients(new ItemStack(Material.WARPED_STEM, 16)).create())
 
 
-                         */
                         .create()
         );
 
         result.add(
                 new VillagerInstance.VillagerBuilder("Carpenter 2", org.bukkit.entity.Villager.Profession.CARTOGRAPHER)
-                  .rarity(Rarity.COMMON)
-                  .proficiency(Proficiency.WOODWORK)
-                  .level(5)
-    /*
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.BONE_MEAL,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.ACACIA_SAPLING,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.BAMBOO,16)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.CHERRY_SAPLING,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.CRIMSON_FUNGUS,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.CRIMSON_NYLIUM,16)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.JUNGLE_SAPLING,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.MANGROVE_PROPAGULE,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.OAK_SAPLING,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.SPRUCE_SAPLING,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.WARPED_FUNGUS,32)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
-                  .add(new MerchantRecipeBuilder(new ItemStack(Material.WARPED_NYLIUM,16)).addIngredients(Items.SILVER_COIN.getItemByKey()).create())
+                        .rarity(Rarity.COMMON)
+                        .proficiency(Proficiency.WOODWORK)
+                        .level(5)
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.BONE_MEAL, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.ACACIA_SAPLING, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.BAMBOO, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.CHERRY_SAPLING, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.CRIMSON_FUNGUS, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.CRIMSON_NYLIUM, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.JUNGLE_SAPLING, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.MANGROVE_PROPAGULE, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.OAK_SAPLING, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.SPRUCE_SAPLING, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.WARPED_FUNGUS, 32))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.WARPED_NYLIUM, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .create()
+        );
 
+        result.add(
+                new VillagerInstance.VillagerBuilder("Carpenter 3", org.bukkit.entity.Villager.Profession.CARTOGRAPHER)
+                        .rarity(Rarity.ADVANCED)
+                        .proficiency(Proficiency.WOODWORK)
+                        .level(10)
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.ACACIA_LOG, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.BAMBOO_BLOCK, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.BIRCH_LOG, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.CHERRY_LOG, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.CRIMSON_STEM, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.JUNGLE_LOG, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.MANGROVE_LOG, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.OAK_LOG, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.SPRUCE_LOG, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .add(new MerchantRecipeBuilder(new ItemStack(Material.WARPED_STEM, 16))
+                                .addIngredients(itemManager.getItemByID(90000).getItemStackRepresentation()).create())
+                        .create()
 
-     */
-                  .create()
         );
 
 
