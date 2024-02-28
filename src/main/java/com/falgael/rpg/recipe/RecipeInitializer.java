962f57afbbd930e0c93d6577612d093b4cd8ba54
falgael <ole.brenner24@gmail.com>
2024-02-28 13:44:15 +0100

    Added Wood recipes for stone cutter

--- src/main/java/com/falgael/rpg/recipe/RecipeInitializer.java
+++ src/main/java/com/falgael/rpg/recipe/RecipeInitializer.java
@@ -1,8 +1,7 @@
 package com.falgael.rpg.recipe;
 
 import com.falgael.rpg.definitions.stonework.recipe.Concrete;
-import com.falgael.rpg.definitions.woodwork.recipe.Hopper;
-import com.falgael.rpg.definitions.woodwork.recipe.Oak;
+import com.falgael.rpg.definitions.woodwork.recipe.*;
 import org.bukkit.plugin.Plugin;
 
 
@@ -10,11 +9,22 @@ import org.bukkit.plugin.Plugin;
 public class RecipeInitializer {
 
     public RecipeInitializer(Plugin plugin) {
-        new Oak(plugin);
-        new Hopper(plugin);
+
         new Concrete(plugin);
 
-
+        //Woodwork
+        new Acacia(plugin);
+        new Bamboo(plugin);
+        new Birch(plugin);
+        new Cherry(plugin);
+        new Crimson(plugin);
+        new DarkOak(plugin);
+        new Hopper(plugin);
+        new Jungle(plugin);
+        new Mangrove(plugin);
+        new Oak(plugin);
+        new Spruce(plugin);
+        new Warped(plugin);
     }
 
 
